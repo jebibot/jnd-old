c3fecf06128e210269a296993a76c4a74b460c87
Jebibot <83044352+jebibot@users.noreply.github.com>
2022-12-29 17:26:32 +0900

    feat: title

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -10,6 +10,7 @@ import { TEAMS } from "../team";
 import PLAYERS from "../data/players.json";
 
 import "react-tooltip/dist/react-tooltip.css";
+import { useEffect } from "react";
 
 export function loader({ request }: LoaderFunctionArgs) {
   return fetch("https://jnd-data.s3.ap-northeast-2.amazonaws.com/data.json", {
@@ -22,13 +23,17 @@ export default function Root() {
   const navigate = useNavigate();
   const location = useLocation();
   const playerKeys = Object.keys(PLAYERS);
+  const player = decodeURIComponent(location.pathname.split("/")[1] || "");
+  useEffect(() => {
+    document.title = `${player || "홈"} | 2023 자낳대 시즌 1 전적`;
+  }, [player]);
 
   return (
     <TooltipProvider>
       <div className="container mx-auto p-2">
         <select
           className="px-4 py-2.5 rounded-full border text-2xl"
-          value={decodeURIComponent(location.pathname.split("/")[1] || "")}
+          value={player}
           onChange={({ target }) => navigate(target.value)}
         >
           <option value="">홈</option>
